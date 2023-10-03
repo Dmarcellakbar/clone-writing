24f851b998bc7e73fa0f01ba10c2f44c9858089f
Dmarcellakbar <dickymarcellino@gmail.com>
2023-10-03 11:24:38 +0700

    update 2

--- src/pages/account/package.tsx
+++ src/pages/account/package.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
-import Stepper from '@/components/Stepper';
+import { Stepper3 } from '@/components/Stepper';
 import Image from 'next/image';
 
 export default function Package() {
@@ -52,61 +52,7 @@ export default function Package() {
                         WritingHero.
                       </div>
                     </div>
-                    <div className="mt-4 mb-8 max-w-lg">
-                      <Stepper
-                        child1={
-                          <li className="flex w-full items-center  after:content-[''] after:w-full after:h-1  after:border-indigo-600 after:border-2 after:inline-block ">
-                            <span className="flex items-center justify-center w-6 h-6 bg-indigo-600 border-2 border-indigo-600 rounded-full lg:h-6 lg:w-6  shrink-0">
-                              <svg
-                                className="h-5 w-5 text-white"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                  clipRule="evenodd"
-                                ></path>
-                              </svg>
-                            </span>
-                          </li>
-                        }
-                        child2={
-                          <li className="flex w-full items-center  after:content-[''] after:w-full after:h-1  after:border-indigo-600 after:border-2 after:inline-block ">
-                            <span className="flex items-center justify-center w-6 h-6 bg-indigo-600 border-2 border-indigo-600 rounded-full lg:h-6 lg:w-6  shrink-0">
-                              <svg
-                                className="h-5 w-5 text-white"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                  clipRule="evenodd"
-                                ></path>
-                              </svg>
-                            </span>
-                          </li>
-                        }
-                        child3={
-                          <li
-                            data-te-stepper-step-ref
-                            className="w-[4.5rem] flex-auto"
-                          >
-                            <span className="flex items-center justify-center w-6 h-6 bg-white border-2 border-indigo-600 rounded-full lg:h-6 lg:w-6  shrink-0">
-                              <span
-                                className="h-2.5 w-2.5 rounded-full bg-indigo-600"
-                                aria-hidden="true"
-                              ></span>
-                            </span>
-                          </li>
-                        }
-                      />
-                    </div>
+                    <Stepper3 />
                     <div className="text-left ">
                       <div className="font-extrabold text-3xl text-black ">
                         Select your prefered package ✨
