0866624f70292e3ad01065b89da918c72535e58e
Dmarcellakbar <dickymarcellino@gmail.com>
2023-10-04 11:26:14 +0700

    update faq

--- src/components/section/Faq.tsx
+++ src/components/section/Faq.tsx
@@ -21,10 +21,10 @@ export default function Pricing() {
           </span>
           <div className="pt-8 space-y-4 ">
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 list-none text-md lg:text-xl text-black font-bold cursor-pointer ">
                 1. What is Writing Hero?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Writing Hero is an AI-based writing software offering over 100
                 tools to help people like you move their business forward at
                 lightning speed! From perfectly crafted emails, ads, to sales
@@ -35,10 +35,10 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none font-bold cursor-pointer">
                 2. How does Writing Hero work?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 You can choose a tool, write a few ideas, and be amazed by the
                 content you can create. Writing Hero has a full-featured
                 document editor, allowing you to work with your writing and
@@ -47,10 +47,10 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none font-bold cursor-pointer">
                 3. What kind of tools does Writing Hero offer?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Writing Hero offers over 100 AI-based writing tools to help you
                 create content for your business. From perfectly crafted emails,
                 ads, to sales letters or full outreach responses, our tools will
@@ -59,10 +59,10 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none font-bold cursor-pointer">
                 4. Is Writing Hero only for businesses?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Writing Hero is designed to cater to a wide range of users,
                 including businesses, solopreneurs, professionals, and
                 individuals who require assistance with their writing tasks.
@@ -73,20 +73,20 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none font-bold cursor-pointer">
                 5. Is there a free trial for Writing Hero?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Yes, Writing Hero offers a limited free account, that allows
                 users to generate 1000 words per month.
               </p>
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none font-bold cursor-pointer">
                 6. What are the pricing plans for Writing Hero?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Writing Hero offers a limited free account, $19 per month for
                 25,000 words generated and an unlimited plan for $29 per month.
                 All users will get the same core functionality, but the only
@@ -98,20 +98,20 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none font-bold cursor-pointer">
                 7. Can I add team members to my Writing Hero account?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Yes, you can add team members to your Writing Hero account for
                 an extra cost.
               </p>
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none font-bold cursor-pointer">
                 8. Can I request new tools for my business?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Yes, Writing Hero allows users to request new tools for their
                 businesses, a feature that is free for all paid users for a
                 limited time.
@@ -119,11 +119,11 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none  font-bold cursor-pointer">
                 9. How do I share my Writing Hero documents with my team or add
                 team members?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Writing Hero allows you to share and collaborate with your team.
                 You can send your team a link to join your Writing Hero account
                 to boost your team’s productivity!
@@ -131,10 +131,10 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none  font-bold cursor-pointer">
                 10. Is the content generated by Writing Hero plagiarism free?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Writing Hero’s AI technology generates virtually plagiarism-free
                 content, but we encourage you to check it before posting it as
                 your own.
@@ -142,21 +142,21 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none  font-bold cursor-pointer">
                 11. Can I regenerate new content if I don’t like what Writing
                 Hero generated?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Yes, you can regenerate new content if you don’t like what
                 Writing Hero generated.
               </p>
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none  font-bold cursor-pointer">
                 12. How do I cancel my Writing Hero subscription?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 You can cancel your Writing Hero subscription by logging into
                 your account and canceling your subscription. Your subscription
                 will continue for the remainder of your billing period. If you
@@ -166,10 +166,10 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none  font-bold cursor-pointer">
                 13. Can I get a refund if I don’t like Writing Hero?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Yes, Writing Hero offers a 7 day money back guarantee if you
                 skipped trying the limited FREE account (1000 words per month vs
                 your current paid plan). We encourage you to try our FREE
@@ -179,10 +179,10 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none  font-bold cursor-pointer">
                 14. Is Writing Hero compatible with mobile devices?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Writing Hero is best used on desktop computers but can also be
                 accessed via mobile devices. However, mobile devices are not
                 optimized for the full experience and we make no guarantee that
@@ -191,10 +191,10 @@ export default function Pricing() {
             </details>
 
             <details className="w-full px-4 bg-white rounded-xl shadow-lg  ">
-              <summary className="px-4 pt-6 pb-4 list-none text-xl font-bold cursor-pointer">
+              <summary className="px-4 pt-6 pb-4 text-md lg:text-xl text-black list-none  font-bold cursor-pointer">
                 15. Is there a customer support team available?
               </summary>
-              <p className=" px-8 pt-2 pb-6 text-left text-lg text-gray-600">
+              <p className=" px-8 pt-2 pb-6 text-left text-sm lg:text-lg text-gray-600">
                 Yes, Writing Hero has a dedicated customer support team
                 available to assist with any questions or issues you may have.
                 You can contact us via email (info@writinghero.ai), or live
